7c81d22500e7264cd3619e4e7de4518037d1a3f6
rahmor <rahmor@yahoo.com>
2019-06-12 12:31:31 -0400

    explore push

---
+++ push-exercise.js
@@ -0,0 +1,23 @@
+'use strict';
+const Array = require('./array-class');
+
+function main() {
+  Array.SIZE_RATIO = 3;
+
+  // Create an instance of the Array class
+  let arr = new Array();
+
+  // Add an item to the array
+  arr.push(3);
+  arr.push(5);
+  arr.push(15);
+  arr.push(19);
+  arr.push(45);
+  arr.push(10);
+
+  console.log(arr);
+}
+
+main();
+
+// Array { length: 6, _capacity: 12, ptr: 3 }
